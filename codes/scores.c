9e795c3943300bd10401bd1e8c839b45ded6e37e
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-13 02:57:59 +0000

    Sat, May 13, 2023, 2:57 AM +00:00

--- codes/scores.c
+++ codes/scores.c
@@ -6,7 +6,7 @@ int main(void)
     int size = 10;
     int scores[] = {73, 71, 33};
     int count = 0;
-    double sum =0 ;
+    double sum = 0 ;
 
     for (int i = 0; i < 10 && scores[i] != 0; i++)
     {

8dbb99353d0c5bec66beab5bff7440a0f9aca35f
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-13 03:19:33 +0000

    Sat, May 13, 2023, 3:19 AM +00:00

--- codes/scores.c
+++ codes/scores.c
@@ -3,19 +3,18 @@
 
 int main(void)
 {
+
     int size = 10;
     int scores[] = {73, 71, 33};
-    int count = 0;
-    double sum = 0 ;
-
-    for (int i = 0; i < size && scores[i] != 0; i++)
+    int count = 0, sum = 0 ;
+    count = sizeof(scores) / sizeof(scores[0]);
+    for (int i = 0 ; i < count ; i++)
     {
         printf("%i\n", scores[i]);
         sum = scores[i] + sum;
-        count++;
     }
 
-    printf("The average is %.2lf\n", sum / count);
+    printf("The average is %.2i\n", sum / count);
 
     return 0;
 

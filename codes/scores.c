eee7201ad8b5ee55b39b1f7f7073629ace35ebc1
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-13 03:50:45 +0000

    Sat, May 13, 2023, 3:50 AM +00:00

--- codes/scores.c
+++ codes/scores.c
@@ -7,21 +7,21 @@ int main(void)
     int size = 10;
     int scores[size];
     int stop = 0;
+    int sum = 0 ;
 
     for (int i = 0; i < size && stop ; i++)
     {
         scores[i] = get_int("Score: ");
     }
-    int sum = 0 ;
-    double count = sizeof(scores) / sizeof(scores[0]);
-    for (int i = 0 ; i < count ; i++)
-    {
-        printf("%i\n", scores[i]);
-        sum = scores[i] + sum;
-    }
+    // double count = sizeof(scores) / sizeof(scores[0]);
+    // for (int i = 0 ; i < count ; i++)
+    // {
+    //     printf("%i\n", scores[i]);
+    //     sum = scores[i] + sum;
+    // }
     //as long as one of the values participating in the arithmetic expression is something like a float
     //the rest will be treated/ promoted as floating value too
-    printf("The average is %.2lf\n", (double)sum / count);
+    // printf("The average is %.2lf\n", (double)sum / count);
 
     return 0;
 

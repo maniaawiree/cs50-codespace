3e48310ba33307dee82b2d4b40f663b46403a215
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-17 23:34:09 +0000

    Wed, May 17, 2023, 11:34 PM +00:00

--- codes/length.c
+++ codes/length.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int string_length(string s);
+
 int main(void)
 {
     string name = get_string("Name: ");
 
+    // int i = 0;
+
+    // while(name[i] != '\0')
+    // {
+    //     i++;
+    // }
+    // printf("%i\n", i);
+
+    int length = string_length(name);
+    printf("%i\n", length);
+
+}
+
+int string_length(string s)
+{
     int i = 0;
-    
-    while(name[i] != '\0')
+
+    while(s[i] != '\0')
     {
         i++;
     }
-    printf("%i\n", i);
-
+    return i;
 }

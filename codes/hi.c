a68484662258a7789201654a8e34cebe307d36f8
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-17 23:02:41 +0000

    Wed, May 17, 2023, 11:02 PM +00:00

--- codes/hi.c
+++ codes/hi.c
@@ -6,6 +6,11 @@ int main(void)
 {
     //a string is an array of characters
     string s = "HI!";
+    string t = "BYE!";
+
     printf("%s\n", s);
+    printf("%s\n", t);
     printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
+    printf("%i %i %i %i %i\n", t[0], t[1], t[2], t[3], t[4]);
+
 }

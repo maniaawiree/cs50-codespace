b898239e82dfae6546dce6b62d91a1da58f11c5f
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-18 00:07:42 +0000

    Thu, May 18, 2023, 12:07 AM +00:00

--- codes/string.c
+++ codes/string.c
@@ -8,16 +8,13 @@ int main(void)
     printf("Output: ");
     //printf("%s\n", s);
     //printing the string inside string s without using s
-    int i;
     /*instead of keep calling strlen as for the for loop condition, we can
     call strlen one time and store the value in a variable!!*/
-    int size = strlen(s);
-    for (i = 0; i < size; i++)
+    for (int i = 0, size = strlen(s); i < size; i++)
     {
+        //now to convert the characters from lower-case to upper-case?
         printf("%c", s[i]);
     }
     putchar('\n');
-    //if i++ for the increment the value of i in the end is 5
-    //if ++i for the increment, the value of i is still 5
-    printf("%i\n", i);
+
 }

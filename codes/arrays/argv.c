d18b5818e15a768f6d60b5bdb90d526de6a77ae5
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-19 01:59:25 +0000

    Fri, May 19, 2023, 1:59 AM +00:00

--- codes/arrays/argv.c
+++ codes/arrays/argv.c
@@ -3,13 +3,22 @@
 
 int main(int argc, string argv[])
 {
-    if(argv[1] == NULL)
+    // if(argv[1] == NULL)
+    // {
+    //     printf("Please enter your name in the command line!!\n");
+    // }
+    // else
+    // {
+    //     printf("Hello, %s\n", argv[1]);
+
+    // }
+
+    if (argc == 2) //checks the number of a
     {
-        printf("Please enter your name in the command line!!\n");
+        printf("Hello, %s\n", argv[1]);
     }
     else
     {
-        printf("Hello, %s\n", argv[1]);
-
+        printf("Hello, World!\n");
     }
 }

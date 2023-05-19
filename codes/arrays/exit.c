bbd03b06730aef97a5813e3f060a10e43f6676e3
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-19 02:11:05 +0000

    Fri, May 19, 2023, 2:11 AM +00:00

--- codes/arrays/exit.c
+++ codes/arrays/exit.c
@@ -3,13 +3,14 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 2) //2 is the name of the program and one more word after that
+    if (argc != 2) //2 is the name of the program and one more word after that
     //like ./argv Mania so that's 2 words
     {
-        printf("Hello, %s\n", argv[1]);
+        printf("Missing command-line argument\n");
+        return 1;
+        \\this is a clue to the system that something went wrong!!
     }
-    else
-    {
-        printf("Hello, World!\n");
-    }
+    //the program will get to this point only if the if condition is false
+        printf("Hello, %s!\n", argv[1]);
+        return 0;
 }

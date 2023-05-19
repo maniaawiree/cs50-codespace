559f22943e979a4a0e40d1d14bca60db9635098f
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-19 02:00:30 +0000

    Fri, May 19, 2023, 2:00 AM +00:00

--- codes/arrays/argv.c
+++ codes/arrays/argv.c
@@ -13,7 +13,8 @@ int main(int argc, string argv[])
 
     // }
 
-    if (argc == 2) //checks the number of a
+    if (argc == 2) //2 is the name of the program and one more word after that
+    //like ./argv Mania so that's 2 words
     {
         printf("Hello, %s\n", argv[1]);
     }

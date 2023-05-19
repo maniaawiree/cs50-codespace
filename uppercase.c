49addb9b29d54890d4d748a2bca29770f480c613
maniaawiree <102549369+maniaawiree@users.noreply.github.com>
2023-05-19 01:12:06 +0000

    Fri, May 19, 2023, 1:12 AM +00:00

--- uppercase.c
+++ uppercase.c
@@ -12,7 +12,18 @@ int main(void)
     call strlen one time and store the value in a variable!!*/
     for (int i = 0, size = strlen(s); i < size; i++)
     {
+        //now to convert the characters from lower-case to upper-case?
+        //the int value of every lower-case letter is 32 more than its upper-case
+        if (s[i] >= 'a' && s[i] <= 'z') //checking if the element is in 'a-z' alphabetical range
+        {
+            printf("%c", s[i] - 32);
+        }
+        //so that other characters are printed the way they are without any changes
+        else
+        {
             printf("%c", s[i]);
+        }
+
     }
     putchar('\n');
 
